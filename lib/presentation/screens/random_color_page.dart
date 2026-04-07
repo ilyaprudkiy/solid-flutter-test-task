320e8f22b492969ef8aeef68fc74227520aede6b
Illia Prudkyi <ilya.prudkiy20@gmail.com>
2026-04-07 17:32:04 +0200

    chore: initialize project structure

---
+++ lib/presentation/screens/random_color_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RandomColorPage extends StatefulWidget {
+const RandomColorPage({super.key});
+
+  @override
+  State<RandomColorPage> createState() => _RandomColorPageState();
+}
+
+class _RandomColorPageState extends State<RandomColorPage> {
+  @override
+  Widget build(BuildContext context) {
+   return const  Scaffold();
+  } 
+  
+}
